ac91c113e1adc8ce5d353e42e5fbccc8bfb57ade
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2025-02-17 23:45:25 +0530

    review

---
+++ Second_Full_Domain/Practicals/Reviews.py
@@ -0,0 +1,24 @@
+nested_list = [1, [2, [3, 4], 5], 6]
+# my_list = []
+# for x in range(len(nested_list)):
+#     if len(str(nested_list[x])) > 1:
+#         for y in range(len(str(nested_list))):
+#             my_list.append(nested_list[x][y])
+#     else:
+#         my_list.append(nested_list[x])
+# print(my_list)
+
+# def func(x,y):
+#     try:
+#         return x/y
+#     except ZeroDivisionError:
+#         return "cannot divide a numbeer by zero"
+    
+# print(func(4,0))
+import time
+curreeent_time = time
+def func(x,y):
+    return x+y
+    
+print(func(4,4))
+print(time.now() - currreeent_time)
