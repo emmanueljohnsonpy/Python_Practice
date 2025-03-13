67d6e37d297b76695b064279947ff56923e2fe84
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2025-03-14 00:03:18 +0530

    class method and static method

---
+++ Second_Full_Domain/Practicals/staticmethod_&_classmethod.py
@@ -0,0 +1,21 @@
+class MathOperations:
+    @staticmethod
+    def add(x, y):
+        return x + y
+        
+print(MathOperations.add(10, 5))
+
+
+
+
+class Profile:
+    name = "Ronaldo"
+    age = 40
+
+    @classmethod
+    def change(cls, name, age):
+        cls.name, cls.age = name, age
+        
+print(Profile.name, Profile.age)
+Profile.change("Messi", 35)
+print(Profile.name, Profile.age)
